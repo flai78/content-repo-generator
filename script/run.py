159d62f8a636d8f0703a94a04596bc1359c511aa
Filippo Lai <filippo.lai@in2.es>
2021-10-25 15:24:28 +0200

    add steps

--- script/run.py
+++ script/run.py
@@ -26,3 +26,10 @@ mydata = data['mydata']
 # printing the output
 print("mydata:%s\n"
       %(mydata))
+
+f = open('./tmp/pkey.txt', 'w+')
+
+for i in range(10):
+     f.write("This is line %d\r\n" % (i+1))
+
+f.close()
