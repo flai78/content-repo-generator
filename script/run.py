6d020e4b7c9840de34916fb7b5dcbe86d6e94280
Filippo Lai <filippo.lai@in2.es>
2021-10-25 15:42:49 +0200

    update

--- script/run.py
+++ script/run.py
@@ -1,5 +1,7 @@
 # importing the requests library
 import requests
+import datetime
+import os
   
 # api-endpoint
 URL = "https://spotest.free.beeceptor.com/api/test"
@@ -18,8 +20,7 @@ r = requests.post(url = URL, json=BODY, headers=HEADERS)
 # extracting data in json format
 data = r.json()
   
-  
-# extracting latitude, longitude and formatted address 
+
 # of the first matching location
 mydata = data['mydata']
   
@@ -28,6 +29,12 @@ print("mydata:%s\n"
       %(mydata))
 
 f = open('./tmp/pkey.txt', 'w+')
+
+update_date = datetime.datetime.now()
+
+f.write(f"Update on {update_date}")
+f.write(f"Data from web service {mydata}")
+f.write(f"Value from env var DATA_KEY {os.getenv('DATA_KEY')}")
 
 for i in range(10):
      f.write("This is line %d\r\n" % (i+1))
